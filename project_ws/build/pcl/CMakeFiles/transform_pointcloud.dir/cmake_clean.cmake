4294a867448faf55c4cdf82e107f9ff45afa7216
CLOliveira294 <coliveira0209@gmail.com>
2025-10-03 12:04:05 -0400

    Project Environment Fully Setup

---
+++ project_ws/build/pcl/CMakeFiles/transform_pointcloud.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/transform_pointcloud.dir/src/transform_pointcloud.cpp.o"
+  "CMakeFiles/transform_pointcloud.dir/src/transform_pointcloud.cpp.o.d"
+  "transform_pointcloud"
+  "transform_pointcloud.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/transform_pointcloud.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
